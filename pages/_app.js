786c961841c0b09fce1cea02969110401804e049
Nick Diamond <nbdiamond@gmail.com>
2022-08-05 13:32:16 -0700

    unoptimized images

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
         <div
           className="pt-4 pb-2 px-10 border-b"
         >
-          <Image src="/icon.svg" height={50} width={50} />
+
         </div>
       </nav>
       <div style={{ height: 'calc(100vh - 172px)', overflow: 'scroll', paddingBottom: 80 }}>
